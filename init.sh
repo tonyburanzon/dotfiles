c6fd37dd3fa19a37d81bf0bc11b175b73d903a3c
Tony Buranzon <tony@Trixie.tigre>
2024-12-05 08:07:54 -0800

    Fix init

--- init.sh
+++ init.sh
@@ -4,17 +4,17 @@ cp .bash* ~
 cp .tmux.conf ~
 
 
-if [ ! -d "~/.local" ]; then
+if [[ ! -d ~/.local ]]; then
   mkdir ~/.local
 fi
 
 cp -r bin ~/.local
 
-if [ ! -d "/.config" ]; then
+if [[ ! -d ~/.config ]]; then
   mkdir ~/.config
 fi
 
-if [[ ! -d "./config/i3" ]]; then
+if [[ ! -d ~/.config/i3 ]]; then
   mkdir ~/.config/i3
 fi
 
